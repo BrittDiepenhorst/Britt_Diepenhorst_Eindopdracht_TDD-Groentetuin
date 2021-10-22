b1d5bbad20e5bce07e45f40e213d22cd62f30805
Britt Diepenhorst <brittdiepenhorst@hotmail.com>
2021-10-22 09:20:22 +0200

    Eindopdracht TDD Groentetuin

--- farm.js
+++ farm.js
@@ -1,23 +1,37 @@
 // functions
 const getYieldForPlant = (plant) => {
-    const result = plant.yield;
-    return result;
+    const yieldForPlant = plant.yield;
+    return yieldForPlant;
 };
 
 const getYieldForCrop = (input) => {
-    const result = (input.numCrops * input.crop.yield);
-    return result;
+    const yieldForCrop = (input.numCrops * input.crop.yield);
+    return yieldForCrop;
 };
 
 const getTotalYield = (crops) => {
-    console.log(crops);
-    const result = (crops.numCrops * crops.crop.yield);
-    return result;
+    const cropsArray = crops.crops;
+    const total = cropsArray.reduce((currentTotal, crop) => {
+        const totalYieldOneCrop = crop.crop.yield * crop.numCrops
+        return totalYieldOneCrop + currentTotal;
+    }, 0)
+    return total;
 };
-getTotalYield();
+
+const getCostsForCrop = (crops) => {
+    console.log(crops)
+    const cropsArray = crops.crops;
+    console.log(cropsArray);
+    const total = cropsArray.reduce((currentTotal, crop) => {
+        const costsForCrop = crop.crop.cost * crop.numCrops;
+        return costsForCrop + currentTotal;
+    }, 0)
+    return total;
+}
 
 module.exports = {
     getYieldForPlant,
     getYieldForCrop,
-    getTotalYield
+    getTotalYield,
+    getCostsForCrop
 };
--- farm.test.js
+++ farm.test.js
@@ -1,4 +1,4 @@
-const { getYieldForPlant, getYieldForCrop, getTotalYield } = require("./farm");
+const { getYieldForPlant, getYieldForCrop, getTotalYield, getCostsForCrop } = require("./farm");
 
 describe("getYieldForPlant", () => {
     const corn = {
@@ -49,5 +49,18 @@ describe("getTotalYield", () => {
         };
         const crops = [{ crop: corn, numCrops: 0 }];
         expect(getTotalYield({ crops })).toBe(0);
+    });
+});
+
+describe("getCostsForCrop", () => {
+    test("getCostsForCrop", () => {
+        const corn = {
+            name: "corn",
+            yield: 30,
+            cost: 1,
+        };
+
+        const crops = [{ crop: corn, numCrops: 5 }];
+        expect(getCostsForCrop({ crops })).toBe(5);
     });
 });
